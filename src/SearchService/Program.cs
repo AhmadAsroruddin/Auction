cf57b42ba104d02cf8609527733ea38707416bbc
Ari <asroruddinahmad09@gmail.com>
2024-04-16 08:34:30 +0700

    Cleaning up SearchService

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
