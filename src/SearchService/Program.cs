e494a3dfe267d7bcdd6d9f416d2734c945509b14
Ahmad Asroruddin <asroruddinahmad09@gmail.com>
2024-04-17 06:06:29 +0700

    initializin moongodb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,20 +1,28 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+await DB.InitAsync("SearchDB", MongoClientSettings
+            .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
 
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.UseAuthorization();
-app.MapControllers();
 
-await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+app.MapControllers();
 
 app.Run();
